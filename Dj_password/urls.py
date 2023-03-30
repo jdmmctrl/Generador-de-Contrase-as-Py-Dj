ca30e990987d61e4e77c91d12e35c8d34f2811b7
jdmmctrl <juandavidmurillomejia0718@outlook.es>
2023-03-30 10:35:21 -0500

    continuacion del proyecto

--- Dj_password/urls.py
+++ Dj_password/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from generator import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home),
-    path('about', views.about),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
     path('password', views.password, name='password')
 ]
